5136660f59c3da993598fdd938aa34fb27ddf721
UltronTheDeath3000 <arina@MacBook-Pro-Arina.local>
2017-02-11 12:50:32 +0300

    Add interface
    1. Добавлена разметка интерфейса;
    2. Добавлена логика на кнопку clear.

--- app/src/main/java/com/example/arina/javacalcapp/Calculator.java
+++ app/src/main/java/com/example/arina/javacalcapp/Calculator.java
@@ -2,12 +2,24 @@ package com.example.arina.javacalcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        this.result = (TextView) findViewById(R.id.result);
+
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("0");
+            }
+        });
     }
 }
